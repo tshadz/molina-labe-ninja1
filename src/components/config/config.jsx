16373ad88218c617d5ba3ee97ea12fa8ae28928a
danilo-ac <anthony.danilo@outlook.com.br>
2021-06-22 18:34:11 -0300

    :package: axios config base  endpoints

---
+++ src/components/config/config.jsx
@@ -0,0 +1,19 @@
+export const baseUrl = "https://labeninjas.herokuapp.com/"
+
+export const headers = {headers: {Authorization: '5c3b1a13-1ed2-4989-bcf4-d7b70f506bf5'}}
+
+// export const auth = {key: 'fd0c113e-8948-4507-8abd-386524fa9ced'}
+
+// const getAllJobs = () => {
+
+//     axios.get(baseUrl+"jobs",auth)
+//     .then ((response)=>{
+//         console.log(response.data)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+// }
+
+// getAllJobs()
